1bdd7b3d550c1a393ca5e3eec35dcb4715972a94
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-17 19:33:11 +0400

    Add project files.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,16 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+
+namespace ConsoleUI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            carManager.Add(new Car());
+            carManager.GetAll();
+        }
+    }
+}
