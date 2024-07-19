ca5ee2e26d8626cee1bfa6e168d7a4f373463f66
elmin007 <imanverdiyevemin414@gmail.com>
2024-07-19 23:47:04 +0400

    Added Migrations.

--- Core/Utilities/Results/Concrete/ErrorDataResult.cs
+++ Core/Utilities/Results/Concrete/ErrorDataResult.cs
@@ -2,19 +2,19 @@
 {
     public class ErrorDataResult<T> : DataResult<T>
     {
-        public ErrorDataResult(T data, string message) : base(data, false, message)
+        public ErrorDataResult(T data, string message) : base(message, false, data)
         {
 
         }
-        public ErrorDataResult(T data) : base(data, false)
+        public ErrorDataResult(T data) : base(false, data)
         {
 
         }
-        public ErrorDataResult(string message) : base(default, false, message)
+        public ErrorDataResult(string message) : base(message, false, default)
         {
 
         }
-        public ErrorDataResult() : base(default, false)
+        public ErrorDataResult() : base(false, default)
         {
 
         }
